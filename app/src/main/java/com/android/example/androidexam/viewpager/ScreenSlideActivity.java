82f360fff1a581e8f774edb641d13d6af877547a
Louis,Park <dntjd0313@gmail.com>
2015-09-16 11:34:57 +0900

    ViewPager + TabLayout 예제

--- app/src/main/java/com/android/example/androidexam/viewpager/ScreenSlideActivity.java
+++ app/src/main/java/com/android/example/androidexam/viewpager/ScreenSlideActivity.java
@@ -2,7 +2,7 @@
 package com.android.example.androidexam.viewpager;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -22,26 +22,36 @@ public class ScreenSlideActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
 
+    private TabLayout mTabLayout;
+
     private List<Fragment> mList;
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_screen_slide);
 
         mViewPager = (ViewPager) findViewById(R.id.viewpager);
 
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mTabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+
         // 짝퉁 데이터
         mList = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             mList.add(new ColorFragment());
+            mTabLayout.addTab(mTabLayout.newTab().setText("Tab" + (i + 1)));
         }
 
         ScreenSlidePagerAdapter adapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(),
                 mList);
 
         mViewPager.setAdapter(adapter);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 
     private class ScreenSlidePagerAdapter extends FragmentPagerAdapter {
